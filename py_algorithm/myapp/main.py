575dc55ef2bceb5503715d0771b549f36a880067
LeoJustFly <823919142@qq.com>
2018-06-06 23:14:01 +0800

    组成员第一次上传代码测试

---
+++ py_algorithm/myapp/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+"""
+@author:dingxiaojie
+@contact:dingxiaojie@huahuan.com
+@file:main
+@time:2018/6/4 10:39
+@desc:
+"""
+
+
+if __name__ == '__main__':
+    print('This is my first time to upload code as a teamer')
+    pass
