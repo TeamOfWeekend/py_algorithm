aa6438674d3d3d52b61b17a9980f52c91eb0fac0
Leo0721 <lipf0627@163.com>
2018-06-07 22:53:30 +0800

    增加二进制手表算法的调用

--- py_algorithm/myapp/main.py
+++ py_algorithm/myapp/main.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 """
-<<<<<<< HEAD
-@author:dingxiaojie
-@contact:dingxiaojie@huahuan.com
-=======
 @author:Leo
 @contact:lipf0627@163.com
->>>>>>> dcc17441b998b77da819bd88df03104d3ccd88c1
 @file:main
 @time:2018/6/4 10:39
 @desc:
 """
 
-
+import BinaryWatch from algo_binary_watch
 if __name__ == '__main__':
-<<<<<<< HEAD
-    print('This is my first time to upload code as a teamer')
-=======
->>>>>>> dcc17441b998b77da819bd88df03104d3ccd88c1
-    pass
+    bit_watch = BinaryWatch()
+    print(bit_watch.getAllBinaryTime(8))
